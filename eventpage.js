c294e3b4e3d608ff0365e3c4c8d4cc97245387cf
Angus <angusdev@gmail.com>
2018-08-29 09:37:33 +0800

    remove x-frame-options
    livescore.com added x-frame-options: deny to prevent the match details from loading in iframe.  Add webRequest listener to remove the x-frame-options header

---
+++ eventpage.js
@@ -0,0 +1,19 @@
+'use strict';
+
+chrome.webRequest.onHeadersReceived.addListener(
+    function(info) {
+        var headers = info.responseHeaders;
+        for (var i=headers.length-1; i>=0; --i) {
+            var header = headers[i].name.toLowerCase();
+            if (header == 'x-frame-options' || header == 'frame-options') {
+                headers.splice(i, 1); // Remove header
+            }
+        }
+        return {responseHeaders: headers};
+    },
+    {
+        urls: [ 'http://*/*' ],
+        types: [ 'sub_frame' ]
+    },
+    ['blocking', 'responseHeaders']
+);
